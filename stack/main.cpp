a0f942793cd38bebf8761bc5826ddd43a7169e84
Keith E. Laidig <klaidig@everettcc.edu>
2010-11-15 08:52:39 -0800

    added to main.cpp

--- stack/main.cpp
+++ stack/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 int main()
 {
 	int ival;
+	double dval;
+	char cval;
 	DynStack<int> istack;
 	DynStack<double> dstack;
 	DynStack<char> cstack;
@@ -22,6 +24,14 @@ int main()
 
 	istack.pop(ival);
 	cout << ival << endl;
+
+	cout << "Double stack\n";
+	for (int i = 1; i < 12; i+=2)
+		dstack.push(static_cast<double>(i));
+
+	dstack.pop(dval);
+	cout << dval << endl;
+	
 	
 	return 0;
 }
